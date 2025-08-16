1084c95592cd1c884a02e1158713eeefc7c83fff
Robin San Martin <robin.sanmartin.venegas@ciisa.cl>
2025-08-16 19:07:00 -0400

    feat/9 added PlayCount

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import './App.css'
-import { PlayListHeader } from './components/PlayListHeader/PlayListHeader'
+import "./App.css";
+import { PlayListHeader } from "./components/PlayListHeader/PlayListHeader";
 
 function App() {
-
   return (
     <>
-    <div>
-      <h1>MUSIC-APP</h1>
-    </div>
+      <div>
+        <h1>MUSIC-APP</h1>
+      </div>
 
       <PlayListHeader
-  title="Mi Playlist Favorita"
-  imageUrl="https://cdn-images.dzcdn.net/images/cover/61274796bc51a91139950cf5c71a5e6a/0x1900-000000-80-0-0.jpg"
-/>
+        title="Mi Playlist Favorita"
+        imageUrl="https://cdn-images.dzcdn.net/images/cover/61274796bc51a91139950cf5c71a5e6a/0x1900-000000-80-0-0.jpg"
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
